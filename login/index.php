d929c9f3c9b4ecfa85fe5af5ba32ede4e04bf1cb
luvumbu <luvumbu@gmail.com>
2023-05-27 15:45:05 +0200

    deplacement de fichier ok

--- login/index.php
+++ login/index.php
@@ -46,10 +46,9 @@ session_start();
 toutes les focti
 --> 
 <script src="class/js/js_off/login_user.js"></script>  
-<script src="class/js/js_array_info/dowload_img.js"></script>  
-<script src="class/js/js_array_info/add_group_affiche_form.js"></script>  
-
-<script src="class/js/js_array_info/add_group.js"></script>  
+<script src="class/js/js_add/dowload_img.js"></script>  
+ 
+ 
 
 
 
