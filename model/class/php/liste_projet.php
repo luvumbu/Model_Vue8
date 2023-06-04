d2f10a3bfa74691ba26f6f867ce53b5d33479590
luvumbu <luvumbu@gmail.com>
2023-06-04 18:18:11 +0200

    ajout blog condition child ok

--- model/class/php/liste_projet.php
+++ model/class/php/liste_projet.php
@@ -60,11 +60,59 @@ $apple1 = new Select_datas($servername,$username,$password,$dbname);
     $apple1->execution();
     $myJSON = json_encode($apple1->list_row); 
 
+
+
+
+   
+
+
+    $apple2 = new Select_datas($servername,$username,$password,$dbname);
+
+  array_push(
+    $apple2->row,
+
+    'liste_projet_id',
+    'liste_projet_id_sha1',
+    'liste_projet_ip',
+    'liste_projet_name',
+    'liste_projet_description1',
+    'liste_projet_description2',
+    'liste_projet_visibilite1',
+    'liste_projet_visibilite2',
+    'liste_projet_type',
+    'information_user_id_sha1',
+    'liste_projet_new_file',
+    'liste_projet_reg_date'  
+    );
+ 
+    $apple2->sql='SELECT * FROM `liste_projet` WHERE `liste_projet_id_parent` ="'.$apple1->list_row[1].'"';
+    $apple2->execution();
+    $myJSON = json_encode($apple2->list_row); 
+
     // echo   $myJSON ; 
  
 
+    
 
-echo $apple1->all_data_json() ; 
+
+
+    if(count($apple1->list_row)>0){
+      echo "[";
+      echo $apple1->all_data_json() ; 
+      echo "," ; 
+      echo $apple2->all_data_json() ; 
+      
+      
+      echo "]";
+    }
+    else {
+      echo '["404"]' ; 
+    }
+
+
+
+
+
  
  
 
