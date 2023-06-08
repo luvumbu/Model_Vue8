80cb46e25f6fd9c4e1280dcb337e805ea916a110
luvumbu <luvumbu@gmail.com>
2023-06-08 23:52:16 +0200

    methode update ok

--- login/class/js/js_array_info/link.js
+++ login/class/js/js_array_info/link.js
@@ -1,4 +1,5 @@
 function link(_this){
+  document.getElementById("id_div_global_style2").innerHTML="" ; 
 
 
   var ok = new Information("class/php/cookie_table/liste_projet_img.php"); // création de la classe 
@@ -44,7 +45,104 @@ function myGreeting() {
 
 
 function myGreetingxx() {
- console.log(array_local[0][0].liste_projet_id) ; 
+
+
+
+var node = document.createElement("div");
+ 
+node.id="myGreetingxx" ;  
+//node.innerHTML="vmax_ok" ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+
+
+
+
+
+var node = document.createElement("input");
+node.id="liste_projet_description1" ;  
+node.value=array_local[0][0].liste_projet_description1 ; 
+node.setAttribute("onkeyup","update_value(this)");
+node.setAttribute("id","i_"+array_local[0][0].liste_projet_id_sha1);
+node.setAttribute("title",array_local[0][0].liste_projet_id_sha1);
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("textarea");
+node.id="liste_projet_description2" ;  
+node.value=array_local[0][0].liste_projet_description2 ; 
+node.setAttribute("onkeyup","update_value(this)");
+node.setAttribute("id","t_"+array_local[0][0].liste_projet_id_sha1);
+node.setAttribute("title",array_local[0][0].liste_projet_id_sha1);
+document.getElementById("id_div_global_style2").appendChild(node);
+
+
+
+
+
+
+var node = document.createElement("div");
+node.id="liste_projet_id" ;  
+node.innerHTML=array_local[0][0].liste_projet_id ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_id_sha1" ;  
+node.innerHTML=array_local[0][0].liste_projet_id_sha1 ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_ip" ;  
+node.innerHTML=array_local[0][0].liste_projet_ip ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_name" ;  
+node.innerHTML=array_local[0][0].liste_projet_name ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+
+
+var node = document.createElement("div");
+node.id="liste_projet_description2" ;  
+node.innerHTML=array_local[0][0].liste_projet_description2 ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_visibilite1" ;  
+node.innerHTML=array_local[0][0].liste_projet_visibilite1 ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_visibilite2" ;  
+node.innerHTML=array_local[0][0].liste_projet_visibilite2 ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_type" ;  
+node.innerHTML=array_local[0][0].liste_projet_type ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="information_user_id_sha1" ;  
+node.innerHTML=array_local[0][0].information_user_id_sha1 ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+
+var node = document.createElement("div");
+node.id="liste_projet_new_file" ;  
+node.innerHTML=array_local[0][0].liste_projet_new_file ;  
+document.getElementById("id_div_global_style2").appendChild(node);
+var node = document.createElement("div");
+node.id="liste_projet_reg_date" ;  
+   
+
+
+ 
+
+
+
+
+
+ 
  console.log(array_local[0][0].liste_projet_id_sha1) ; 
  console.log(array_local[0][0].liste_projet_ip) ; 
  console.log(array_local[0][0].liste_projet_name) ; 
@@ -57,6 +155,13 @@ function myGreetingxx() {
  console.log(array_local[0][0].liste_projet_new_file) ; 
  console.log(array_local[0][0].liste_projet_reg_date) ; 
 
+
+ console.log(array_local[1]) ; 
+ console.log(array_local[1].length) ; 
+
+
+
+ 
  
  
  
