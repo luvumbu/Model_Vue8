52151aabd34df70a8a72f7412f7ce2a98ba7711f
luvumbu <luvumbu@gmail.com>
2023-06-25 08:27:46 +0200

    voir tous les projet ok

--- projet.php
+++ projet.php
@@ -19,29 +19,7 @@ header("Access-Control-Allow-Origin: *");
  
  
 
- $name ="" ; 
- $eman ="" ; 
-
-
-
-
- for($i = 0 ; $i<strlen($_SERVER['PHP_SELF']) ; $i ++){
-
  
-  if(strrev($_SERVER['PHP_SELF'])[$i]=="/") {
-    break ;
-  }
-  else {
-    $eman =  $eman .strrev($_SERVER['PHP_SELF'])[$i] ; 
-  }
- }
-
-
-  
-
-
-
- $url = strrev($eman);
 
 
 
@@ -49,61 +27,7 @@ header("Access-Control-Allow-Origin: *");
   
 include("model/class/php/Select_datas.php") ;  
 
-$eman="";
-$name="";
-
-
-
- for($i=strlen($_SERVER['PHP_SELF'])-1; $i>-1;$i--){
-    
-     if($_SERVER['PHP_SELF'][$i]=='/'){
-       break ; 
-     }
-     $eman = $eman.$_SERVER['PHP_SELF'][$i] ; 
- }
-
- $eman = $eman;
-
-
-
- for($i=strlen($eman)-1; $i>-1;$i--){
-    
-
-
- $name = $name.$eman[$i] ;
-
-}
-
-
-
-
-
  
-
-
-
-$apple = new Select_datas($servername,$username,$password,$dbname);
-
- array_push(
-   $apple->row,
-
-   'liste_projet_description1',
-   'liste_projet_img',
-   'liste_projet_description2', 
-   'liste_projet_reg_date',
-   'liste_projet_visibilite1',
-   'liste_projet_id_sha1' 
-   
-
-   );
-
-   $id_information_user_sha1 =$_SESSION["information_user_id_sha1"] ; 
-   $apple->sql='SELECT * FROM `liste_projet` WHERE `liste_projet_id_sha1`="'.$url.'" ';
-   $apple->execution();
-   $myJSON = json_encode($apple->list_row); 
-
-   // echo   $myJSON ; 
-
 
 
 
@@ -130,7 +54,7 @@ $apple = new Select_datas($servername,$username,$password,$dbname);
    );
 
    $id_information_user_sha1 =$_SESSION["information_user_id_sha1"] ; 
-   $apple2->sql='SELECT * FROM `liste_projet` WHERE `liste_projet_id_parent`="'.$name.'"';
+   $apple2->sql='SELECT * FROM `liste_projet` WHERE `liste_projet_id_parent`=""';
    $apple2->execution();
    $myJSON = json_encode($apple2->list_row); 
 
@@ -146,125 +70,6 @@ $apple = new Select_datas($servername,$username,$password,$dbname);
 
 
 
-echo '<div class="page_t1">';
-
-for($i = 0 ; $i <count($apple->list_row) ; $i ++ ){
-// echo $apple->list_row[$i].'<br/>' ; 
-
-// echo(fmod($i, count($apple->row)) . "<br>");
-
- 
-
- switch (fmod($i, count($apple->row))) {
-   case 0:
-  
-    
- 
- if($apple->list_row[$i+4]==""){
-
-   echo '<h1>'.$apple->list_row[$i].'</h1>' ; 
- }
-
-     
-
-     break;
-   case 1:
-
-    if($apple->list_row[$i+3]==""){
-     
-      if($apple->list_row[$i]==""){
-        echo '<img src="https://images.pexels.com/photos/3705944/pexels-photo-3705944.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" srcset="">' ; 
- 
-      }
-      else {
-
-
-
-        if($_SERVER['SERVER_NAME']=="localhost"){
-          echo '<img src="http://localhost/Model_Vue8/login/src/img/all/'.$apple->list_row[$i].'" alt="" srcset="">' ; 
-
-        }
-        else {
-  
-
-          echo '<div>';
-          echo '<img src="https://'.$_SERVER['SERVER_NAME'].'/login/src/img/all/'.$apple->list_row[$i].'">' ; 
-          echo '</div>';
-          
- 
- 
-        }
-
- 
-      }
-
-
-    }
-     
-
-     break;
-
-     case 2:
-  
-    if($apple->list_row[$i+2]==""){
-
-      echo '<h3>'.$apple->list_row[$i].'</h3>' ; 
-
-    }
- 
-      
-       break;
-
-
-       case 3:
- 
- 
-        if($_SERVER['SERVER_NAME']=="localhost"){
-          if($apple->list_row[$i+2]==""){
-
-            echo "<img src='http://localhost/Model_Vue8/login/src/img/all/qr_code/".$apple->list_row[$i+2].".png'>" ; 
-
-          }
-
-        }
-        else {
-
-          if($apple->list_row[$i+1]==""){
-            echo '<img src="https://'.$_SERVER['SERVER_NAME'].'/login/src/img/all/qr_code/'.$apple->list_row[$i+2].'.png">' ; 
-
-
-          }
-
-          
-  
-          
- 
- 
-        }
-
-
-
-        if($apple->list_row[$i+1]==""){
-        echo '<h4>'.$apple->list_row[$i].'</h4>' ; 
-
-        }
-
-      
-          
-           break;
-           
-
-
- }
-
-
-
-
-
-
-}
-
-echo '</div>';
 
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -300,7 +105,7 @@ for($i = 0 ; $i <count($apple2->list_row) ; $i ++ ){
 
   
       if($apple2->list_row[$i+3]==""){
-        echo "<div><a href='".$apple2->list_row[$i+4]."'>Voir article</a>
+        echo "<div><a href='vlog.php/".$apple2->list_row[$i+4]."'>Voir article</a>
 
         </div>" ; 
        
@@ -390,7 +195,7 @@ for($i = 0 ; $i <count($apple2->list_row) ; $i ++ ){
 
  <h1 class="voir_projet">
    
-  <a href="../projet.php">Voir tous les projet</a>
+  <a href="projet.php">Voir tous les projet</a>
 
 
  </h1>
